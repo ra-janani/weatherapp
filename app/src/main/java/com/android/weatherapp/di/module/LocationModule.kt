4e00bb08af84362bd02848ca3a754719f4f9f3fd
Janani Rajendran <ra.janani@yahoo.co.in>
2023-05-31 18:05:01 +0100

    added missing files

---
+++ app/src/main/java/com/android/weatherapp/di/module/LocationModule.kt
@@ -0,0 +1,20 @@
+package com.android.weatherapp.di.module
+
+import com.android.weatherapp.data.repo.DefaultLocationTracker
+import com.android.weatherapp.util.LocationTracker
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import javax.inject.Singleton
+
+@ExperimentalCoroutinesApi
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class LocationModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindLocationTracker(defaultLocationTracker: DefaultLocationTracker): LocationTracker
+}
---
+++ app/src/main/java/com/android/weatherapp/util/LocationTracker.kt
@@ -0,0 +1,7 @@
+package com.android.weatherapp.util
+
+import android.location.Location
+
+interface LocationTracker {
+    suspend fun getCurrentLocation(): Location?
+}
